0ebb77c4d9e6aade21fbd7779232ec034794e4cd
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-20 10:58:08 +0500

    связываю

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace L_R_2_Khasanova_BPI_23_01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
