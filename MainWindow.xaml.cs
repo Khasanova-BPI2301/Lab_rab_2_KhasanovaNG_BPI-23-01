0460d593bc602b0a3dc676d710ec021136fc42a9
Khasanova <nurgiza.hasanova@yandex.ru>
2025-10-21 04:36:55 +0500

    заполнила кнопки

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using L_R_2_Khasanova_BPI_23_01.Classes;
 
 namespace L_R_2_Khasanova_BPI_23_01
 {
@@ -16,9 +17,35 @@ namespace L_R_2_Khasanova_BPI_23_01
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FunctionCalculator calculator;
         public MainWindow()
         {
             InitializeComponent();
+
+            calculator = FunctionCalculator();
+
+            R1ComboF.Items.Add(4);
+            R1ComboF.Items.Add(5);
+            R1ComboF.Items.Add(6);
+            R1ComboF.Items.Add(7);
+            R1ComboF.Items.Add(8);
+            R1ComboF.Items.Add(9);
+
+            R2ComboF.Items.Add(10);
+            R2ComboF.Items.Add(20);
+            R2ComboF.Items.Add(30);
+            R2ComboF.Items.Add(40);
+
+            R3ComboC.Items.Add(0);
+            R3ComboC.Items.Add(1);
+
+            R3ComboD.Items.Add(-1);
+            R3ComboD.Items.Add(0);
+            R3ComboD.Items.Add(1);
         }
+
+        
+
+       
     }
 }
